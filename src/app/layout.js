32863981b989e527529828ad9ed435a8d2268f50
Mahliyo <gasandigital13@gmail.com>
2026-07-12 10:05:13 +0500

    struktura header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,23 @@
-
 import "./globals.css";
 import Header from "@/Components/Header/Header";
 import Footer from "@/Components/Footer/Footer";
-import { LanguageProvider } from "@/i18n/LanguageProvider";
+import { LanguageProvider } from "@/i18n/LanguageProvider"; 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="uz">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>AssiTech</title>
+      </head>
       <body>
+    
         <LanguageProvider>
-          <Header/>
+          <Header />
           <main>{children}</main>
           <Footer />
         </LanguageProvider>
-      
       </body>
     </html>
   );
