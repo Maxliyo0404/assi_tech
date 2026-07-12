169b2cb75e3a042301f12a4f34c784a0b7ad64d4
Mahliyo <gasandigital13@gmail.com>
2026-07-12 09:27:49 +0500

    struktura header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,10 @@
 import React, { use } from 'react'
 import "./Header.css";
+"use client";
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+
+    </div>
   )
 }
