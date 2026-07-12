f201d8986139ee3c30fb0491dc3505526e9072a3
Mahliyo <gasandigital13@gmail.com>
2026-07-13 00:00:43 +0500

    contact

--- src/Components/Home/Supply.jsx
+++ src/Components/Home/Supply.jsx
@@ -1,10 +1,14 @@
 'use client';
-import { SUPPLY_DATA } from "../lib/data";
-import { useLanguage } from "@/i18n/LanguageProvider"; 
+
+import { SUPPLY_DATA, TRANSLATIONS } from "../lib/data"; 
+import { useLanguage } from "@/i18n/LanguageProvider";
 import Image from "next/image";
 
 export default function SupplySection() {
-  const { lang } = useLanguage(); // Hozirgi til (ru, en, uz)
+  const { lang } = useLanguage(); 
+
+  // Agar til aniqlanmasa, default 'en' ni olamiz
+  const t = TRANSLATIONS[lang] || TRANSLATIONS["en"];
 
   return (
     <section className="py-20 bg-white w-full">
@@ -12,18 +16,18 @@ export default function SupplySection() {
         {SUPPLY_DATA.map((item) => (
           <div key={item.id} className="flex flex-col md:flex-row items-center justify-between w-full mb-24">
             
-            {/* Matn qismi (w-full va text-left) */}
             <h2 className="text-[45px] font-bold text-[#000080] leading-[1.1] w-full md:w-[60%] text-left">
-              <span className="text-red-600">{item.content[lang].highlight}</span>{" "}
-              {item.content[lang].text}
+              <span className="text-red-600">
+                {t[item.titleKey]}
+              </span>{" "}
+              {t[item.textKey]}
             </h2>
 
-            {/* Rasm qismi (ID bo'yicha) */}
             {item.image && (
               <div className="relative w-full md:w-[400px] h-[300px] mt-8 md:mt-0 shrink-0">
                 <Image 
                   src={item.image} 
-                  alt={item.content[lang].highlight} 
+                  alt={t[item.titleKey]} 
                   fill 
                   className="object-contain" 
                 />
