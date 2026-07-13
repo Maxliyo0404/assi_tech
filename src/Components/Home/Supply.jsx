d14d3a58e92a5236e38a48a782a82e54c01baf92
Mahliyo <gasandigital13@gmail.com>
2026-07-13 23:06:56 +0500

    about

--- src/Components/Home/Supply.jsx
+++ src/Components/Home/Supply.jsx
@@ -12,26 +12,27 @@ export default function SupplySection() {
 
 
  return (
-    <section className="py-16 bg-white w-full">
+    <section className="py-12 bg-white w-full">
+      {/* Container hamma narsani markazlaydi va chetlarini yig'adi */}
       <div className="container mx-auto px-4">
         {SUPPLY_DATA.map((item) => (
           <div 
             key={item.id} 
-            className={`flex flex-col md:flex-row items-center justify-between mb-20 gap-10 ${
+            className={`flex flex-col md:flex-row items-center justify-between mb-12 gap-6 ${
               item.reverse ? "md:flex-row-reverse" : ""
             }`}
           >
-            {/* Matn qismi: max-w bilan chekladik, rasmga yopishib qolmaydi */}
-            <div className="w-full md:w-1/2 max-w-[600px]">
+            {/* Matn qismi: w-1/2 orqali kenglikni chekladik */}
+            <div className="w-full md:w-1/2">
               <h2 className="text-[32px] md:text-[45px] font-bold text-[#000080] leading-[1.1] text-left">
                 <span className="text-red-600">{t(item.titleKey)}</span>{" "}
                 {t(item.textKey)}
               </h2>
             </div>
 
-            {/* Rasm qismi */}
+            {/* Rasm qismi: Container ichida qoladi */}
             {item.image && (
-              <div className="w-full md:w-[400px] h-[300px] relative shrink-0">
+              <div className="w-full md:w-[400px] h-[250px] relative">
                 <Image 
                   src={item.image} 
                   alt={t(item.titleKey)} 
